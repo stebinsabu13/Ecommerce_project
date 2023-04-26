a21432721cc9b69db41c6ce1428b16b59d205935
Stebin Sabu <stebinsabu369@gmail.com>
2023-04-26 13:02:37 +0530

    Comments cleared

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -11,11 +11,13 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+// Password hashing
 func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
 	return string(bytes), err
 }
 
+// password authorization
 func CheckPasswordHash(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
@@ -46,11 +48,13 @@ func LoginHandler(c *gin.Context) {
 		c.JSON(http.StatusUnauthorized, "Invalid Password")
 		return
 	}
+	//calling function to get the token string
 	tknstr, err := middlewares.GenerateToken(user.Email)
 	if err != nil {
 		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "Not able to generate token"})
 		return
 	}
+	//token string set to the cookie
 	c.SetCookie("token", tknstr, int(time.Now().Add(30*time.Minute).Unix()), "/", "localhost", false, true)
 	c.Redirect(http.StatusFound, "/home")
 }
@@ -71,61 +75,3 @@ func LogoutHandler(c *gin.Context) {
 	c.SetCookie("token", "", -1, "/", "localhost", false, true)
 	c.JSON(http.StatusOK, gin.H{"logout": "Success"})
 }
-
-// package controllers
-
-// import (
-// 	"net/http"
-// 	"sportzone/initializers"
-// 	"sportzone/models"
-
-// 	"github.com/gin-gonic/gin"
-// 	"golang.org/x/crypto/bcrypt"
-// )
-
-// func HashPassword(password string) (string, error) {
-// 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
-// 	return string(bytes), err
-// }
-
-// func CheckPasswordHash(password, hash string) bool {
-// 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
-// 	return err == nil
-// }
-
-// func LoginHandler() gin.HandlerFunc {
-// 	return func(c *gin.Context) {
-// 		var user models.User
-// 		_, err := c.Cookie("token")
-// 		if err == nil {
-// 			c.Redirect(http.StatusFound, "/home")
-// 			return
-// 		}
-// 		var body struct {
-// 			Email    string
-// 			Password string
-// 		}
-// 		if err := c.BindJSON(&body); err != nil {
-// 			c.JSON(http.StatusBadRequest, "Error to bind JSON format")
-// 			return
-// 		}
-// 		initializers.DB.Where("email=?", body.Email).Find(&user)
-// 		if user.ID == 0 {
-// 			c.JSON(http.StatusUnauthorized, "Invalid Email address")
-// 			return
-// 		}
-// 		ok := CheckPasswordHash(body.Password, user.Password)
-// 		if !ok {
-// 			c.JSON(http.StatusUnauthorized, "Invalid Password")
-// 			return
-// 		}
-// 		c.Set("user_email", user.Email)
-// 	}
-// }
-
-// func HomeHandler(c *gin.Context) {
-// 	user, ok := c.Get("user")
-// 	if ok {
-// 		c.JSON(http.StatusOK, user)
-// 	}
-// }
--- middlewares/token.go
+++ middlewares/token.go
@@ -33,11 +33,12 @@ func VerifyToken() gin.HandlerFunc {
 			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Claims not parsed"})
 		}
 		if token.Valid {
+			//checking the expiry of the token
 			if time.Now().Unix() > claims.ExpiresAt.Unix() {
 				c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Token expired"})
 			}
 			var user models.User
-
+			//Finding the user
 			initializers.DB.Where("email=?", claims.UserName).Find(&user)
 
 			if user.ID == 0 {
@@ -56,20 +57,3 @@ func GenerateToken(userName string) (tokenString string, err error) {
 	tokenString, err = token.SignedString(jwtkey)
 	return
 }
-
-// func GenerateToken(c *gin.Context) {
-// 	user_email, ok := c.Get("user_email")
-// 	if !ok {
-// 		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized User"})
-// 	}
-// 	expirationTime := time.Now().Add(30 * time.Minute)
-// 	claims := Claims{UserName: user_email, RegisteredClaims: jwt.RegisteredClaims{ExpiresAt: jwt.NewNumericDate(expirationTime)}}
-// 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-// 	tknstr, err := token.SignedString(jwtkey)
-// 	if err != nil {
-// 		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "Not able to generate token"})
-// 		return
-// 	}
-// 	c.SetCookie("token", tknstr, int(time.Now().Add(30*time.Minute).Unix()), "/", "localhost", false, true)
-// 	c.Redirect(http.StatusFound, "/home")
-// }
